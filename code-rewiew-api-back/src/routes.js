aa59342f60fefd0dfeb730942ddfb3257be2dd6e
SAMu3lms <samuelmacielsts@gmail.com>
2023-09-23 19:34:46 -0300

    delete de arquivos do back

--- code-rewiew-api-back/src/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const UsuariosController = require('./Controllers/UsuariosController');
-
-router.post('/usuarios', UsuariosController.cadastrarUsuario);
-router.post('/validarUsuario', UsuariosController.validarUsuario);
-
-module.exports = router;
