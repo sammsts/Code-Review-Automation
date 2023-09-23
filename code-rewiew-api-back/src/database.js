5ebd2423506323c415dc754ad1c50fbb574eaeb2
SAMu3lms <samuelmacielsts@gmail.com>
2023-09-23 19:34:46 -0300

    delete de arquivos do back

--- code-rewiew-api-back/src/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('Code-Review-Automation', 'postgres', '123456', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-module.exports = sequelize;
