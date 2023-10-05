a041ce52be84c98b89d84187538c063c1a054676
SAMu3lms <samuelmacielsts@gmail.com>
2023-10-05 14:54:15 -0300

    Removendo backend.

--- code-review-back/debug-node.js/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const UsuariosController = require('../../controllers/UsuariosController.js');
-router.get('/UsuariosController/validarUsuario', UsuariosController.validarUsuario);
-
-
-module.exports = router;
